57c6bd535b3b82199159653dc5be7a7fedd60e89
elia benedetti <eliabenedetti04@gmail.com>
2023-11-21 10:22:07 +0100

    quick update

--- app/controllers/emotions_controller.rb
+++ app/controllers/emotions_controller.rb
@@ -1,8 +1,21 @@
 class EmotionsController < ApplicationController
   def index
+    @emotions = Emotion.all
+    # filter for emotions by universalEmotion
+    # if params[:universalEmotion]
+    #   universalEmotion = params[:universalEmotion]
+    #   emotion = Emotion.find_by(universalEmotion: universalEmotion)
+    #   @emotions = [emotion]
+    # end
+    # filter for emotions by universalEmotion
+    if params[:universalEmotion]
+      universal_emotion = params[:universalEmotion]
+      @emotions = Emotion.where(universalEmotion: universal_emotion)
+    end
   end
 
   def show
+    @emotion = Emotion.find(params[:id])
   end
 
   def new
