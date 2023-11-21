865772d490512b5f02aa34eb72759bcea1e0d0a3
Andrea Zambrano <andreazambrano16@gmail.com>
2023-11-21 14:57:35 +0100

    fixed bugs

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
   root to: "emotions#index"
-  resources :emotions
+
+  resources :emotions do
+    resources :bookings, except: :destroy
+  end
+
+  resources :bookings, only: :destroy
+
   devise_for :users
   resources :users
-  resources :bookings
 end
