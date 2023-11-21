cf1001b10368f0f2155d8b969fd05a9e3edddfa4
elia benedetti <eliabenedetti04@gmail.com>
2023-11-21 13:01:47 +0100

    added emotions htmls and styled a tiny bit, added 5 universal emotions and added seeds for emotions

--- app/controllers/emotions_controller.rb
+++ app/controllers/emotions_controller.rb
@@ -1,28 +1,47 @@
 class EmotionsController < ApplicationController
+  before_action :set_emotion, only: %i[show edit update destroy]
+
   def index
     @emotions = Emotion.all
     # filter for emotions by universalEmotion
     if params[:universalEmotion]
-      universal_emotion = params[:universalEmotion]
+      universal_emotion = params[:universal_emotion_id]
       @emotions = Emotion.where(universalEmotion: universal_emotion)
     end
   end
 
-  def show
-    @emotion = Emotion.find(params[:id])
-  end
+  def show; end
 
   def new
     @emotion = Emotion.new
+    @user = User.find(params[:id])
   end
 
   def create
     @emotion = Emotion.new(emotion_params)
+    @emotion.save
+    redirect_to root_path
+  end
+
+  def edit; end
+
+  def update
+    @emotion.update(emotion_params)
+    redirect_to root_path
+  end
+
+  def destroy
+    @emotion.destroy
+    redirect_to root_path
   end
 
   private
 
+  def set_emotion
+    @emotion = Emotion.find(params[:id])
+  end
+
   def emotion_params
-    params.require(:emotion).permit(:name, :universalEmotion, :description)
+    params.require(:emotion).permit(:name, :universal_emotion_id, :description)
   end
 end
