eea591ceccaf3317429c423e1035d091784f1359
elia benedetti <eliabenedetti04@gmail.com>
2023-11-23 12:20:16 +0100

    added photos for emotions and for users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -39,6 +39,6 @@ class UsersController < ApplicationController
   end
 
   def user_params
-    params.require(:user).permit(:first_name, :last_name, :email, :password, :photo)
+    params.require(:user).permit(:first_name, :last_name, :email, :password)
   end
 end
